ea9b0d8c3a8a38789406384afbf5e82ff5f96fd0
Maxim Zhukov <Maxim Zhukov>
2016-10-18 12:41:59 +0300

    lo

---
+++ Function.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab._1
+{
+    class Function
+    {
+        public static string Factorial(int n)
+        {
+            int resault = 1;
+            string nothing = "Не допустиме чісло!!";
+            if (n > 1 && n <= 9)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    resault *= i;
+                }
+                return Convert.ToString(resault); ;
+            }
+            else
+            {
+                return nothing;
+            }
+            
+        } 
+    }
+ }
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Function.Factorial(s));
+            Console.ReadKey();
+        }
+    }
+}
